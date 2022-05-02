aab7e0dba84e0d2f66a953d0aff3e01129710472
Janae <45527013+snowwDay@users.noreply.github.com>
2022-05-02 00:04:33 -0500

    Add files via upload

---
+++ app/src/main/java/edu/shsu/hci/mynotesapp/ThirdFragment.java
@@ -0,0 +1,50 @@
+package edu.shsu.hci.mynotesapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import edu.shsu.hci.mynotesapp.databinding.FragmentThirdBinding;
+
+public class ThirdFragment extends Fragment {
+
+    private FragmentThirdBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentThirdBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonThird.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ThirdFragment.this)
+                        .navigate(R.id.action_thirdFragment_to_FirstFragment);
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
+}
